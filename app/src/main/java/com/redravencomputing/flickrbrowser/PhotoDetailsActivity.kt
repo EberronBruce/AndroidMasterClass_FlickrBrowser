059d3f39dfb5105eea5d85b66facc68a6b2f1dc4
Bruce <bruce@redravencomputing.com>
2021-02-10 04:49:39 -0700

    Update images on details activity

--- app/src/main/java/com/redravencomputing/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/redravencomputing/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,13 +1,32 @@
 package com.redravencomputing.flickrbrowser
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 
 class PhotoDetailsActivity : BaseActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
+
         activateToolbar(true)
 
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        val photo_title = findViewById<TextView>(R.id.photo_title)
+        val photo_tags = findViewById<TextView>(R.id.photo_tags)
+        val photo_author = findViewById<TextView>(R.id.photo_author)
+        val photo_image = findViewById<ImageView>(R.id.photo_image)
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.get().load(photo.link)
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(photo_image)
     }
 }
